dd1a8262a4bffc42ae117f3a2440b9a757a682d9
Bhupendrakumar Piprava <bdpiparva@gmail.com>
2019-09-06 14:07:44 +0530

    Get current position from the location including pathname

--- source/javascripts/app/_version_helper.js
+++ source/javascripts/app/_version_helper.js
@@ -1,14 +1,24 @@
-$(function() {
-  var currentUrlContains = function(needle){
+function getCurrentPosition() {
+  var currentHash = window.location.hash;
+  var currentPath = window.location.pathname;
+  if (currentPath) {
+    // get the complete pathname after the version i.e. "19.8.0/foo/bar/baz" => "foo/bar/baz"
+    return currentPath.split("/", 3)[2] + currentHash;
+  }
+  return currentHash;
+}
+
+$(function () {
+  var currentUrlContains = function (needle) {
     return window.location.href.indexOf(needle) >= 0;
   };
 
   // this URL is not available at development time, but it's available on production.
   // If you change this JS, make sure it does not break switching on old pages.
   var xhr = $.ajax('/versions.json');
-  xhr.done(function(data, textStatus, jqXHR) {
+  xhr.done(function (data, textStatus, jqXHR) {
     var versionSwitcher = $('.version-switcher select');
-    $(data).each(function(index, elem) {
+    $(data).each(function (index, elem) {
       var optionText = elem.version;
 
       if (elem.type) {
@@ -27,13 +37,12 @@ $(function() {
       versionSwitcher.append(optionElem);
     });
 
-    versionSwitcher.on('change', function(e) {
-      var currentPosition = window.location.hash;
+    versionSwitcher.on('change', function (e) {
       var selectedElem = $(e.target).find(":selected");
       var location = selectedElem.attr('data-url');
       var oldSite = selectedElem.attr('data-oldsite');
       if (!oldSite) {
-        location = location + currentPosition;
+        location = location + getCurrentPosition();
       }
       window.location.href = location;
     });
